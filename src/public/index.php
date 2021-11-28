6fb8a6f94e07d0ba0853e7c5bb9b78b65ebe0f95
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2021-11-28 19:54:50 +0900

    ルーティング設定

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,27 @@
 <?php
-echo 'Hello World!';
+
+// @see https://github.com/php-fig/http-message/blob/master/docs/PSR7-Interfaces.md
+// @see https://www.php-fig.org/psr/psr-7/#psrhttpmessageserverrequestinterface
+use \Psr\Http\Message\ServerRequestInterface as Request;
+
+// @see https://www.php-fig.org/psr/psr-7/#psrhttpmessageresponseinterface
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+  $response->getBody()->write("OK");
+  return $response;
+});
+
+$app->get('hello/{name}', function (Request $request, Response $response, array $args) {
+  $name = $args['name'];
+  $response->getBody()->write("Hello, $name");
+  return $response;
+});
+
+$app->run();
+
