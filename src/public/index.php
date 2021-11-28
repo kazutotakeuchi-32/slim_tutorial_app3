3b93ebed0a4ec73820996d9e16af43cef627b01c
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2021-11-28 21:19:28 +0900

    slim/views 追加

--- src/public/index.php
+++ src/public/index.php
@@ -7,7 +7,11 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 // @see https://www.php-fig.org/psr/psr-7/#psrhttpmessageresponseinterface
 use \Psr\Http\Message\ResponseInterface as Response;
 
+// @see https://github.com/slimphp/PHP-View
+use Slim\Views\PhpRenderer;
+
 require '../../vendor/autoload.php';
+
 
 $app = new \Slim\App;
 
@@ -22,6 +26,14 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
   $response->getBody()->write("Hello, $name");
   return $response;
 });
+
+$app->get("/ticket/{id}", function (Request $request, Response $response, array $args) {
+  $ticektId = (int)$args['id'];
+  $response->getBody()->write("Ticket $ticektId");
+  return $response;
+});
+
+
 
 $app->run();
 
